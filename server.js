5b99617eabb3686ffe6046080e3d177eaf9ed241
Ubuntu <ubuntu@ip-172-31-16-156.us-west-2.compute.internal>
2015-04-30 06:18:11 +0000

    Fix js loading, removes node_modules, adds README

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var mongoose        = require('mongoose');
 var db = require('./config/db');
 
 // Set up port
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 9000;
 
 // Connect to our mongoDB
 // uncomment after you enter in credentials to config/db.js
