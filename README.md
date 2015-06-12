ec2aacc659d6c18e245d11fdfa21ba69444d10f5
Michael Sawyer <michaelsawyer92@gmail.com>
2015-06-11 22:05:37 -0700

    Fixes scope issue with rsvp object.

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 * Integrate with paypal.
-* Change icons.
-* First date instead of Michael's section.
 * Ceremony & Reception Details.
 * Hotel arrangements. (Maybe reserve a block of rooms)
--- public/js/controllers/rsvp.js
+++ public/js/controllers/rsvp.js
@@ -1,20 +1,19 @@
 var weddingApp = angular.module('weddingApp', []);
 
 weddingApp.controller('RsvpController', ['$scope','$http', function ($scope, $http) {
-  var home          = $scope;
-  $scope.author           = "Michael Sawyer";
-  $scope.rsvp             = {};
-  $scope.rsvp.name        = '';
-  $scope.rsvp.count       = 0;
-  $scope.rsvp.message     = '';
-  $scope.rsvp.email       = '';
-  $scope.rsvp.menuBeef    = 0;
-  $scope.rsvp.menuChicken = 0;
-  $scope.rsvp.menuVeggie  = 0;
+  var home                = this;
+  home.author           = "Michael Sawyer";
+  home.rsvp             = {};
+  home.rsvp.name        = '';
+  home.rsvp.count       = 0;
+  home.rsvp.message     = '';
+  home.rsvp.email       = '';
+  home.rsvp.menuBeef    = 0;
+  home.rsvp.menuChicken = 0;
+  home.rsvp.menuVeggie  = 0;
 
   this.submitRsvp = function() {
     $http.post("/api/guests", home.rsvp).success(function(data, status) {
-      var payload = home.rsvp;
       home.rsvp.name        = '';
       home.rsvp.count       = 0;
       home.rsvp.message     = '';
@@ -22,7 +21,8 @@ weddingApp.controller('RsvpController', ['$scope','$http', function ($scope, $ht
       home.rsvp.menuBeef    = 0;
       home.rsvp.menuChicken = 0;
       home.rsvp.menuVeggie  = 0;
-      return payload;
+    }).error(function(data, status) {
+        console.log("That sucks...")
     })
   };
 
